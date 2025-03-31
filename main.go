052f2280f187d25504f1e9e2b0db628366d9b9ec
thanawat <tkittisupjaroen@gmail.com>
2025-04-01 01:24:04 +0700

    logic and output pass

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"maps"
+
+	"net/http"
 
 	"github.com/g-thanawat/product_format/pd_format"
-	// "github.com/labstack/echo/v4"
+	// "github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 // Example of struct
@@ -27,27 +30,100 @@ type CleanedOrder struct {
 	TotalPrice float64 `json:"totalPrice"`
 }
 
-func textureCount(s string) map[string]int {
-	r := map[string]int{}
-	word := strings.Fields(s)
-	for _, v := range word {
-		r[v] = r[v] + 1
+type Add_ons struct {
+	No         int     `json:"no"`
+	ProductId  string  `json:"productId"`
+	Qty        int     `json:"qty"`
+	UnitPrice  float64 `json:"unitPrice"`
+	TotalPrice float64 `json:"totalPrice"`
+}
+
+func order(c echo.Context) error {
+
+	od := []InputOrder{}
+	err := c.Bind(&od)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "Data not complete")
 	}
-	return r
+
+	Texture_amount := map[string]int{}
+
+	var No_pd int = 0
+	var pd_amount int = 0
+	var total_pd_amount int = 0
+
+	var output []any
+
+	for _, pd := range od {
+		product := pd_format.Clean_and_split(pd.PlatformProductId)
+		new_texture_amount := pd_format.TextureCount(product, Texture_amount)
+		maps.Insert(Texture_amount, maps.All(new_texture_amount))
+
+		//Cal unit Price
+		for _, v := range product {
+			pd_amount += v.Quantity
+		}
+
+		for _, data := range product {
+
+			var Order = CleanedOrder{}
+			No_pd += 1
+			Order.No = No_pd
+			Order.ProductId = data.FilmID + "-" + data.TextureID + "-" + data.Phone_modelID
+			Order.MaterialId = data.FilmID + "-" + data.TextureID
+			Order.ModelId = data.Phone_modelID
+			Order.Qty = data.Quantity
+			Order.UnitPrice = pd.TotalPrice / float64(pd_amount)
+			Order.TotalPrice = Order.UnitPrice * float64(Order.Qty)
+
+			output = append(output, Order)
+		}
+
+		//Total product amount
+		total_pd_amount += pd_amount
+		//clear product amount
+		pd_amount = 0
+
+	}
+
+	fmt.Printf("Texture = %#v \n", Texture_amount)
+
+	//Add ons
+
+	//Add wipingcloth
+	No_pd += 1
+	wipingcloth := Add_ons{No_pd, "WIPING-CLOTH", total_pd_amount, 0.00, 0.00}
+	output = append(output, wipingcloth)
+
+	//Add the others
+	for t := range maps.Keys(Texture_amount) {
+		No_pd += 1
+		Cleaner := Add_ons{No_pd, t + "-CLEANER", Texture_amount[t], 0.00, 0.00}
+		output = append(output, Cleaner)
+	}
+
+	return c.JSON(http.StatusCreated, output)
 }
 
 func main() {
-	// e := echo.New()
+	e := echo.New()
 
-	// e.GET("/", endpoint)
 	fmt.Println("hello")
 
-	// clean_and_split("FG0A-CLEAR-IPHONE16PROMAX"
+	// clean_and_split("FG0A-CLEAR-IPHONE16PROMAX")
 
 	// clean_and_split("--FG0A-CLEAR-OPPOA3*2/FG0A-MATTE-OPPOA3")
 	// fmt.Println("------------------------------------------------------------------------\n")
-	d := pd_format.Clean_and_split("--FG0A-CLEAR-OPPOA3*2/FG0A-MATTE-OPPOA3*2")
-	fmt.Printf("Final : %#v\n", d)
+	// d := pd_format.Clean_and_split("--FG0A-CLEAR-OPPOA3*2/FG0A-MATTE-OPPOA3*2")
+	// d := pd_format.Clean_and_split("x2-3&FG0A-MATTE-IPHONE16PROMAX*3")
+	// fmt.Printf("Final : %#v\n", d)
 
-	// e.Start("localhost:3000")
+	// mm := map[string]int{}
+	// a := pd_format.TextureCount(d, mm)
+	// fmt.Printf("FinalTexture : %#v\n", a)
+	// maps.Insert(mm, maps.All(a))
+	// fmt.Printf("FinalTexture2 : %#v\n", mm)
+	e.POST("/order", order)
+	e.Start("localhost:3000")
 }
--- pd_format/pd_format.go
+++ pd_format/pd_format.go
@@ -2,6 +2,7 @@ package pd_format
 
 import (
 	"fmt"
+	"maps"
 	"strconv"
 	"strings"
 )
@@ -10,11 +11,13 @@ func Add(a, b int) int {
 	return a + b
 }
 
+var Texture_amount map[string]int
+
 type Product_Code_Format struct {
 	FilmID        string
-	textureID     string
-	phone_modelID string
-	quantity      int
+	TextureID     string
+	Phone_modelID string
+	Quantity      int
 }
 
 func Clean_and_split(s string) []Product_Code_Format {
@@ -47,13 +50,13 @@ func Clean_and_split(s string) []Product_Code_Format {
 
 			if err != nil {
 				fmt.Println("Not number\n")
-				p_format.quantity = 1
+				p_format.Quantity = 1
 			} else {
-				p_format.quantity = val
+				p_format.Quantity = val
 			}
 
 		} else {
-			p_format.quantity = 1
+			p_format.Quantity = 1
 		}
 
 		// Split each ID
@@ -62,16 +65,26 @@ func Clean_and_split(s string) []Product_Code_Format {
 
 		// Fill each data
 		p_format.FilmID = data[0]
-		p_format.textureID = data[1]
-		p_format.phone_modelID = data[2]
+		p_format.TextureID = data[1]
+		p_format.Phone_modelID = data[2]
 
 		result = append(result, p_format)
 
 	}
 
-	// fmt.Printf("SPLIT : %#v\n", result)
+	fmt.Printf("SPLIT : %#v\n", result)
 
 	return result
+}
+
+func TextureCount(p []Product_Code_Format, m map[string]int) map[string]int {
+	// r := map[string]int{}
+	old := maps.Clone(m)
+	// old := map[string]int{}
+	for _, v := range p {
+		old[v.TextureID] = old[v.TextureID] + v.Quantity
+	}
+	return old
 }
 
 func main() {
